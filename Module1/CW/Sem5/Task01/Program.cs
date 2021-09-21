41abf713cb4b5d1fbf81cb0cff4dcd223e69d09a
apbesshapov <apbesshapov@edu.hse.ru>
2021-09-21 20:58:42 +0300

    CW 5

---
+++ Module1/CW/Sem5/Task01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Sums(uint number, out uint sumEven, out uint sumOdd)
+        {
+            sumEven = 0;
+            sumOdd = 0;
+            bool chet = true;
+            while (number != 0)
+            {
+                if (chet)
+                {
+                    sumEven += number % 10;
+                    chet = false;
+                }
+                else
+                {
+                    sumOdd += number % 10;
+                    chet = true;
+                }
+                number /= 10;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Module1/CW/Sem5/Task05/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task05
+{
+    class Program
+    {
+        static double S(uint k)
+        {
+            double sum = 0;
+            for (int i = 1; i < k+1; i++)
+            {
+                sum += ((double)i + 0.3) / ((double)(3*i*i) + 5.0);
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
