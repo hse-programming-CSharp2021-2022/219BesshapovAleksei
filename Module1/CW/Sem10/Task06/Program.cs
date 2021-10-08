d8f7f0fc96fdede2c621e5269102e65e3d2c03d1
apbesshapov <apbesshapov@edu.hse.ru>
2021-10-08 14:48:20 +0300

    Seminar 10

---
+++ Module1/CW/Sem10/Task06/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task06
+{
+    class Program
+    {
+        static void Filling(int n, double percent)
+        {
+            char[] mas = new char[n];
+            int letterAmount = (int)(n * percent / 100.0);
+            Random random = new Random();
+            for (int i = 0; i < letterAmount; i++)
+            {
+                mas[i] = (char)random.Next((int)('a'), (int)('z') + 1);
+            }
+            for (int i = letterAmount; i < n; i++)
+            {
+                mas[i] = (char)random.Next('0', '9' + 1);
+            }
+            Array.ForEach(mas, el => Console.Write(el + " "));
+        }
+        static void Main(string[] args)
+        {
+            int n = 10;
+            double percent = 30;
+            Filling(n, percent);   
+        }
+    }
+}
---
+++ Module1/CW/Sem10/Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static string Line(int[] series)
+        {
+            string res = "";
+            string[] letters = { "ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять" };
+            foreach (var item in series)
+            {
+                if (item >= 0 && item <= 9)
+                {
+                    int index = int.Parse(item.ToString());
+                    res += letters[index];
+                }
+                else
+                {
+                    res += item.ToString();
+                }
+                res += " ";
+            }
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            int[] mas = new int[10];
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                mas[i] = random.Next(0, 10);
+            }
+            Console.WriteLine(Line(mas));
+        }
+    }
+}
