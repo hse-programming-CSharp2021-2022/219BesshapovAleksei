2c7fd9c7743fed0d8959aeb41e2e63649ed13d76
apbesshapov <apbesshapov@edu.hse.ru>
2021-10-12 21:02:46 +0300

    Seminar 11

---
+++ Module1/CW/Sem11/Task03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            StringBuilder sb;
+            string[] lines = new string[6];
+            for (int i = 0; i < 6; i++)
+            {
+                sb = new StringBuilder();
+                int length = random.Next(20, 51);
+                for (int j = 0; j < length; j++)
+                {
+                    sb.Append((char)random.Next('a', 'z' + 1));
+                }
+                sb.Append(".");
+                lines[i] = sb.ToString();
+            }
+            File.WriteAllLines("data2.txt", lines);
+            string[] allLines = File.ReadAllLines("data2.txt");
+            foreach (var item in allLines)
+            {
+                Console.WriteLine(item.Substring(0, item.Length - 1));
+            }
+        }
+    }
+}
