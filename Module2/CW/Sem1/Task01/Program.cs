5b9bae1d72d38573b422670c88f485c5bf4747a5
apbesshapov <apbesshapov@edu.hse.ru>
2021-10-26 20:47:05 +0300

    Module 2 started, CW 1

---
+++ Module2/CW/Sem1/Task01/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Task01
+{
+    class Nangler
+    {
+        int n;
+        decimal r;
+        public Nangler(int amount=4, decimal rad=1)
+        {
+            n = amount;
+            r = rad;
+        }
+        public decimal P
+        {
+            get
+            {
+                return 2 * r * (decimal)Math.Sin(Math.PI / (double)n) / (decimal)Math.Cos(Math.PI / (double)n) * n;
+            }
+        }
+        public decimal S
+        {
+            get
+            {
+                return this.P / 2 * r;
+            }
+        }
+        public string PolygonData()
+        {
+            string res = "Количество сторон: " + n.ToString() + ", радиус: " + r.ToString() + ", P: " + this.P +
+                ", S: " + this.S;
+            return res;
+        }
+    }
+    class Program
+    {
+        static int SortByS(Nangler a, Nangler b)
+        {
+            if (a.S < b.S)
+            {
+                return 1;
+            }
+            else if (a.S > b.S)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Nangler[] nanglers = new Nangler[n];
+            for (int i = 0; i < n; i++)
+            {
+                int amount = int.Parse(Console.ReadLine());
+                decimal rad = decimal.Parse(Console.ReadLine());
+                nanglers[i] = new Nangler(amount, rad);
+            }
+            decimal[] ps = new decimal[n];
+            decimal[] ss = new decimal[n];
+            for (int i = 0; i < n; i++)
+            {
+                ps[i] = nanglers[i].P;
+                ss[i] = nanglers[i].S;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{i + 1}-й n - угольник: {nanglers[i].PolygonData()}");
+            }
+            Array.Sort(ss);
+            Console.WriteLine("Минимальная площадь: " + ss[0]);
+            Console.WriteLine("Максимальная: " + ss[n - 1]);
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                nanglers[i] = new Nangler(random.Next(1, 180), random.Next(1, 1000));
+            }
+            for (int i = 0; i < n; i++)
+            {
+                ps[i] = nanglers[i].P;
+                ss[i] = nanglers[i].S;
+            }
+            Array.Sort(nanglers, SortByS);
+            foreach (var item in nanglers)
+            {
+                Console.WriteLine(item.PolygonData());
+            }
+        }
+    }
+}
