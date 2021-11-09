fd1ab231f39ac45124412b9ac236e80b8e2ed57c
apbesshapov <apbesshapov@edu.hse.ru>
2021-11-10 01:29:23 +0300

    HW5 Module2

---
+++ Module2/HW/HW_5/Cinderella/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Cinderella
+{
+    abstract class Something { }
+    class Lentil : Something
+    {
+        double Weight
+        {
+            get;
+        }
+        public Lentil()
+        {
+            Random random = new Random();
+            this.Weight = 2 * random.NextDouble();
+        }
+        public override string ToString()
+        {
+            return $"Lentil, volume = {this.Weight}";
+        }
+    }
+    class Ashes : Something
+    {
+        double Volume
+        {
+            get;
+        }
+        public Ashes()
+        {
+            Random random = new Random();
+            this.Volume = random.NextDouble();
+        }
+        public override string ToString()
+        {
+            return $"Ashes, volume = {this.Volume}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Random random = new Random();
+            Something[] things = new Something[n];
+            for (int i = 0; i < n; i++)
+            {
+                int choice = random.Next(0, 2);
+                things[i] = choice == 0 ? new Lentil() : new Ashes();
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(things[i]);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(things[i].GetType().ToString() == "Cinderella.Ashes" ? things[i] :"");
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(things[i].GetType().ToString() != "Cinderella.Ashes" ? things[i] : "");
+                Console.Write(" ");
+            }
+        }
+    }
+}
---
+++ Module2/HW/HW_5/Task04/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Task04
+{
+    class Shape
+    {
+        public const double PI = Math.PI;
+        protected double _x, _y;
+        public Shape()
+        {
+        }
+        public Shape(double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
+        public virtual double Area()
+        {
+            return _x * _y;
+        }
+    }
+    class Circle : Shape
+    {
+        public Circle(double r)
+        {
+            _x = r;
+        }
+        public override double Area()
+        {
+            return PI * this._x * this._x;
+        }
+        public override string ToString()
+        {
+            return $"Тип фигуры: круг, площадь: {this.Area()}";
+        }
+    }
+    class Cylinder : Shape
+    {
+        public Cylinder(double r, double h)
+        {
+            _x = r;
+            _y = h;
+        }
+        public override double Area()
+        {
+            return 2 * PI * this._x * this._x + 2 * PI * this._x * this._y;
+        }
+        public override string ToString()
+        {
+            return $"Тип фигуры: цилиндр, площадь: {this.Area()}";
+        }
+    }
+    class Sphere : Shape
+    {
+        public Sphere(double r)
+        {
+            _x = r;
+        }
+        public override double Area()
+        {
+            return 4 * PI * this._x * this._x;
+        }
+        public override string ToString()
+        {
+            return $"Тип фигуры: сфера, площадь: {this.Area()}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int n1 = random.Next(3, 6);
+            int n2 = random.Next(3, 6);
+            int n3 = random.Next(3, 6);
+            Shape[] shapes = new Shape[n1 + n2 + n3];
+            for (int i = 0; i < n1; i++)
+            {
+                shapes[i] = new Circle(random.Next(5, 15));
+            }
+            for (int i = n1; i < n1 + n2; i++)
+            {
+                shapes[i] = new Cylinder(random.Next(5, 15), random.Next(5, 15));
+            }
+            for (int i = n1 + n2; i < shapes.Length; i++)
+            {
+                shapes[i] = new Sphere(random.Next(5, 15));
+            }
+            for (int i = 0; i < shapes.Length; i++)
+            {
+                Console.WriteLine($"Фигура {i + 1}, площадь: {shapes[i].Area()}.");
+            }
+            for (int i = 0; i < shapes.Length; i++)
+            {
+                if (shapes[i] is Shape a)
+                {
+                    Console.WriteLine(a);
+                }
+            }
+        }
+    }
+}
