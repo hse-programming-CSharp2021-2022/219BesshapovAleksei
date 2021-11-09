33cd2442b2aaf4e49d5d0bc642be1798e7d62cbb
apbesshapov <apbesshapov@edu.hse.ru>
2021-11-10 00:41:42 +0300

    HW4 Module2

---
+++ Module2/HW/HW_4/Task03/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace Task03
+{
+    class RedisClient
+    {
+        public const uint MaxCount = 5;
+        private static ConnectionMultiplexer redis;
+        private static IDatabase database;
+
+        public static void Connect(string connectionString = "redis-19933.c1.us-west-2-2.ec2.cloud.redislabs.com:19933,password=OFvYs20jHXNJu5xRgHh979jywCRwoXe2,allowAdmin=true")
+        {
+            redis = ConnectionMultiplexer.Connect(connectionString);
+            database = redis.GetDatabase();
+        }
+
+        public static string Get(string key)
+        {
+            return Exist(key) ? database.ListGetByIndex(key, database.ListLength(key) - 1) : "Данных не существует"; 
+        }
+
+        public static bool Exist(string key)
+        {
+            return database.KeyExists(key);
+        }
+
+        public static void Add(string key, string value)
+        {
+            if (Exist(key))
+            {
+                if ((uint)database.ListLength(key) < MaxCount)
+                {
+                    database.ListRightPush(key, value);
+                }
+                else
+                {
+                    Console.WriteLine("Предельное количество записей");
+                }
+            }
+            else
+            {
+                database.ListRightPush(key, value);
+            }
+        }
+
+        public static string Back(string key)
+        {
+            database.ListRightPop(key);
+            if (database.ListLength(key) != 0)
+            {
+                return database.ListGetByIndex(key, database.ListLength(key) - 1);
+            }
+            else
+            {
+                database.KeyDelete(key);
+                return "Данные удалены";
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RedisClient.Connect();
+            string request = Console.ReadLine();
+            string[] nameAndSettings = request.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            while (nameAndSettings[0] != "exit")
+            {
+                try
+                {
+                    if (nameAndSettings[0] == "add")
+                    {
+                        RedisClient.Add(nameAndSettings[1], nameAndSettings[2]);
+                    }
+                    if (nameAndSettings[0] == "back")
+                    {
+                        Console.WriteLine(RedisClient.Back(nameAndSettings[1]));
+                    }
+                    if (nameAndSettings[0] == "get")
+                    {
+                        Console.WriteLine(RedisClient.Back(nameAndSettings[1]));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                finally
+                {
+                    request = Console.ReadLine();
+                    nameAndSettings = request.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                }
+            }
+        }
+    }
+}
---
+++ Module2/HW/HW_4/Task04/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace Task04
+{
+    class RedisClient
+    {
+        private static ConnectionMultiplexer redis;
+        private static IDatabase database;
+
+        public static void Connect(string connectionString = "redis-19933.c1.us-west-2-2.ec2.cloud.redislabs.com:19933,password=OFvYs20jHXNJu5xRgHh979jywCRwoXe2,allowAdmin=true")
+        {
+            redis = ConnectionMultiplexer.Connect(connectionString);
+            database = redis.GetDatabase();
+        }
+
+        public static string Get(string key)
+        {
+            return database.KeyExists(key) ? database.StringGet(key) : "Склада нет";
+        }
+
+        public static bool Exist(string key, string value)
+        {
+            if (database.KeyExists(key))
+            {
+                return database.StringGet(key).ToString().Contains(value);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void Add(string key, string value)
+        {
+            if (database.KeyExists(key))
+            {
+                database.StringAppend(key, $" {value}");
+            }
+            else
+            {
+                database.StringAppend(key, $"{value}");
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RedisClient.Connect();
+            string request = Console.ReadLine();
+            string[] nameAndSettings = request.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            while (nameAndSettings[0] != "exit")
+            {
+                try
+                {
+                    if (nameAndSettings[0] == "add")
+                    {
+                        RedisClient.Add(nameAndSettings[1], nameAndSettings[2]);
+                    }
+                    if (nameAndSettings[0] == "get")
+                    {
+                        Console.WriteLine(RedisClient.Get(nameAndSettings[1]));
+                    }
+                    if (nameAndSettings[0] == "exist")
+                    {
+                        Console.WriteLine(RedisClient.Exist(nameAndSettings[1], nameAndSettings[2]));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                finally
+                {
+                    request = Console.ReadLine();
+                    nameAndSettings = request.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                }
+            }
+        }
+    }
+}
