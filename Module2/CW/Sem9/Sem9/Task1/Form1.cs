b814efe02595b1df97b8353c4811b76f2c68f41e
apbesshapov <apbesshapov@edu.hse.ru>
2021-11-23 19:41:36 +0300

    Seminar 9

---
+++ Module2/CW/Sem9/Sem9/Task1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = "Нажал";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.button2.Visible = false;
+            this.button3.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.button2.Visible = true;
+            this.button3.Visible = false;
+            this.button2.Text = "Оп";
+        }
+    }
+}
