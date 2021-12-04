1d5821479032250f5ff696a684023d8950be421a
apbesshapov <apbesshapov@edu.hse.ru>
2021-12-05 00:53:20 +0300

    HW trackbar drawing

---
+++ Module2/HW/HW_TrackbarDrawing/TrackbarDrawing_XAML/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrackbarDrawing_XAML
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.polyline.Points.Add(new Point(this.sliderX.Value, 200 - this.sliderY.Value));
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            this.polyline.Points.Clear();
+        }
+    }
+}
