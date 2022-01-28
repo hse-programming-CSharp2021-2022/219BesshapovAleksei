b50e5c95c3d8d8fc2cd3e4135f9ac7b46d985f2c
apbesshapov <apbesshapov@edu.hse.ru>
2022-01-28 15:58:35 +0300

    Sem6

---
+++ Module3/CW/Sem6/Task02/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Task02
+{
+    class Point
+    {
+        public double X
+        {
+            get;
+            set;
+        }
+        public double Y
+        {
+            get;
+            set;
+        }
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public static double GetDistance(Point A, Point B)
+        {
+            return Math.Sqrt(Math.Pow(A.X - B.X, 2) + Math.Pow(A.Y - B.Y, 2));
+        }
+    }
+    class TriangleComp
+    {
+        Point[] points;
+        double[] lengths;
+        public double S
+        {
+            get
+            {
+                double p = (lengths[0] + lengths[1] + lengths[2]) / 2;
+                return Math.Sqrt(p * (p - lengths[0]) * (p - lengths[1]) * (p - lengths[2]));
+            }
+        }
+        public double P
+        {
+            get
+            {
+                return lengths[0] + lengths[1] + lengths[2];
+            }
+        }
+        public TriangleComp(double x1, double y1, double x2, double y2, double x3, double y3)
+        {
+            points = new Point[] { new Point(x1, y1), new Point(x2, y2), new Point(x3, y3)};
+            lengths = new double[] { Point.GetDistance(points[0], points[1]), Point.GetDistance(points[0], points[2]), Point.GetDistance(points[1], points[2]) };
+        }
+        public TriangleComp(Point A, Point B, Point C)
+        {
+            points = new Point[] { A, B, C };
+            lengths = new double[] { Point.GetDistance(points[0], points[1]), Point.GetDistance(points[0], points[2]), Point.GetDistance(points[1], points[2]) };
+        }
+        public bool IsInTriangle(Point A)
+        {
+            TriangleComp newTriangle = new TriangleComp(points[0].X, points[0].Y, points[1].X, points[1].Y, A.X, A.Y);
+            return S > newTriangle.S; 
+        }
+        public override string ToString()
+        {
+            return $"Периметр: {this.P}, Площадь: {this.S}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomizer = new Random();
+            TriangleComp[] triangles = new TriangleComp[5];
+            for (int i = 0; i < 5; i++)
+            {
+                double x1 = 10 * randomizer.NextDouble();
+                double x2 = 10 * randomizer.NextDouble();
+                double y1 = 10 * randomizer.NextDouble();
+                double y2 = 10 * randomizer.NextDouble();
+                double x3 = 10 * randomizer.NextDouble();
+                double y3 = 10 * randomizer.NextDouble();
+                triangles[i] = new TriangleComp(x1, y1, x2, y2, x3, y3);
+            }
+            Array.ForEach(triangles, el => Console.WriteLine(el));
+            Array.Sort(triangles, (a, b) => -a.S.CompareTo(b.S));
+            Array.ForEach(triangles, el => Console.WriteLine(el));
+            Array.Sort(triangles, (a, b) => a.P.CompareTo(b.P));
+            Array.ForEach(triangles, el => Console.WriteLine(el));
+        }
+    }
+}
---
+++ Module3/CW/Sem6/TaskIAnimalable/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace TaskIAnimalable
+{
+    abstract class Animal : IComparable
+    {
+        public int Age
+        {
+            get;
+            set;
+        }
+        public int CompareTo(object? animal2)
+        {
+            Animal newAnimal = animal2 as Animal;
+            return this.Age.CompareTo(newAnimal.Age);
+        }
+    }
+    interface IJumpable
+    {
+        void Jump();
+    }
+    interface IRunnable
+    {
+        void Run();
+    }
+    class Cocoroach : Animal, IRunnable, IComparable
+    {
+        public double Speed
+        {
+            get;
+            set;
+        }
+        public void Run()
+        {
+            Console.WriteLine($"Cocoroach runs with speed of {this.Speed}");
+        }
+        public Cocoroach(int age, double speed)
+        {
+            Age = age;
+            Speed = speed;
+        }
+        public override string ToString()
+        {
+            return $"Таракан: возраст {this.Age}, скорость: {this.Speed}";
+        }
+        /*
+        public new int CompareTo(object? cocroach)
+        {
+            Cocoroach newCocroach = cocroach as Cocoroach;
+            return newCocroach.Speed.CompareTo(this.Speed);
+        }
+        */
+    }
+    class Cangaroo : Animal, IJumpable, IComparable
+    {
+        public double HeightOfJump
+        {
+            get;
+            set;
+        }
+        public void Jump()
+        {
+            Console.WriteLine($"Cangaroo jumps with height of {this.HeightOfJump}");
+        }
+        public Cangaroo(int age, double height)
+        {
+            Age = age;
+            HeightOfJump = height;
+        }
+        public override string ToString()
+        {
+            return $"Кангуру: возраст {this.Age}, высота прыжка {this.HeightOfJump}";
+        }
+        /*
+        public new int CompareTo(object? cangaroo)
+        {
+            Cangaroo newCangaroo = cangaroo as Cangaroo;
+            return newCangaroo.HeightOfJump.CompareTo(this.HeightOfJump);
+        }
+        */
+    }
+    class Jeopardy : Animal, IRunnable, IJumpable
+    {
+        public double Speed
+        {
+            get;
+            set;
+        }
+        public double HeightOfJump
+        {
+            get;
+            set;
+        }
+        public void Run()
+        {
+            Console.WriteLine($"Jeopardy runs with speed of {this.Speed}");
+        }
+        public void Jump()
+        {
+            Console.WriteLine($"Jeopardy jumps with height of {this.HeightOfJump}");
+        }
+        public Jeopardy(int age, double speed, double height)
+        {
+            Age = age;
+            Speed = speed;
+            HeightOfJump = height;
+        }
+        public override string ToString()
+        {
+            return $"Гепард: возраст {this.Age}, скорость {this.Speed}, высота прыжка {this.HeightOfJump}"; ;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Animal[] animals = new Animal[6] { new Cocoroach(random.Next(1, 5), random.NextDouble()), new Cocoroach(random.Next(1, 5), random.NextDouble()), new Cangaroo(random.Next(1, 10), random.Next(1, 3)), new Cangaroo(random.Next(1, 10), random.Next(1, 3)), new Jeopardy(random.Next(1, 8), random.Next(5, 10), random.Next(1, 2)), new Jeopardy(random.Next(1, 8), random.Next(5, 10), random.Next(1, 2))};
+            Array.ForEach(animals, el => Console.WriteLine(el));
+            IJumpable[] jumpers = new IJumpable[4];
+            int amount = 0;
+            foreach (var animal in animals)
+            {
+                if (animal is IJumpable)
+                {
+                    jumpers[amount] = (IJumpable)animal;
+                    amount++;
+                }
+            }
+            Console.WriteLine("***");
+            Array.ForEach(jumpers, el => el.Jump());
+            Console.WriteLine("***");
+            IRunnable[] runners = new IRunnable[4];
+            amount = 0;
+            foreach (var animal in animals)
+            {
+                if (animal is IRunnable)
+                {
+                    runners[amount] = (IRunnable)animal;
+                    amount++;
+                }
+            }
+            Array.ForEach(runners, el => el.Run());
+            Console.WriteLine("***");
+            Array.Sort(animals);
+            Array.ForEach(animals, el => Console.WriteLine(el));
+            Console.WriteLine("***");
+        }
+    }
+}
