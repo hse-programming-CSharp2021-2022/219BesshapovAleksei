fb6e030dba0a8698e266b7a09626938ca2882c18
apbesshapov <apbesshapov@edu.hse.ru>
2022-02-15 19:34:06 +0300

    Sem11

---
+++ Module3/CW/Sem11/Task02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fs = new FileStream("Task02.txt", FileMode.OpenOrCreate))
+            {
+                if (fs.Length < 3)
+                {
+                    byte[] bytes = { (byte)((byte)'A' + fs.Length) };
+                    fs.Position = fs.Length;
+                    fs.Write(bytes);
+                }
+            }
+            using (FileStream fs = new FileStream("Task02.txt", FileMode.Open))
+            {
+                int next = 0;
+                while (next != -1)
+                {
+                    next = fs.ReadByte();
+                    byte[] read = { (byte)next };
+                    if (next == -1)
+                    {
+                        break;
+                    }
+                    Console.Write(System.Text.Encoding.Default.GetString(read) + " ");
+                }
+            }
+        }
+    }
+}
---
+++ Module3/CW/Sem11/TaskBinaryWriter/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+namespace TaskBinaryWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            using (BinaryWriter writer = new BinaryWriter(File.Open("Numbers.txt", FileMode.OpenOrCreate)))
+            {
+                for (int i = 0; i < 10; i++)
+                    writer.Write(random.Next(1, 101));
+            }
+            int[] array = new int[10];
+            using (BinaryReader reader = new BinaryReader(File.Open("Numbers.txt", FileMode.OpenOrCreate)))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    array[i] = reader.ReadInt32();
+                    Console.WriteLine(array[i]);
+                }
+                int n = int.Parse(Console.ReadLine());
+                int min = -1, res = -1;
+                for (int i = 0; i < 10;i++)
+                {
+                    if (i == 0)
+                    {
+                        res = 0;
+                        min = Math.Abs(n - array[i]);
+                    }
+                    else
+                    {
+                        min = Math.Min(Math.Abs(n - array[i]), min);
+                        res = i;
+                    }
+                }
+                array[res] = n;
+            }
+            using (BinaryWriter writer = new BinaryWriter(File.Open("Numbers.txt", FileMode.OpenOrCreate)))
+            {
+                for (int i = 0; i < 10;i++)
+                    writer.Write(array[i]);
+            }
+            using (BinaryReader reader = new BinaryReader(File.Open("Numbers.txt", FileMode.OpenOrCreate)))
+            {
+                for (int i = 0; i < 10;i++)
+                {
+                    array[i] = reader.ReadInt32();
+                    Console.Write(array[i] + " ");
+                }
+            }
+        }
+    }
+}
