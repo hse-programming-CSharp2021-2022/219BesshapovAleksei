41ba3bcf34f31182af62ee5052cf70f71516614e
apbesshapov <apbesshapov@edu.hse.ru>
2022-02-18 14:28:11 +0300

    Sem12 + HWUni

---
+++ Module3/CW/Sem12/Task01/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace Task01
+{
+    [Serializable]
+    public class Student
+    {
+        public string Surname
+        {
+            get;
+            set;
+        }
+        public int CourseNumber
+        {
+            get;
+            set;
+        }
+        public Student(string surname, int number)
+        {
+            Surname = surname;
+            CourseNumber = number;
+        }
+        public Student() : this("Иванов", 1)
+        {
+            return;
+        }
+        public override string ToString()
+        {
+            return $"{this.Surname}, {this.CourseNumber};";
+        }
+    }
+    [Serializable]
+    public class Group
+    {
+        public string GroupName
+        {
+            get;
+            set;
+        }
+        public List<Student> Students
+        {
+            get;
+            set;
+        }
+        public Group(string group, List<Student> students)
+        {
+            GroupName = group;
+            Students = students;
+        }
+        public Group() : this("БПИ219", new List<Student>() { new Student("Бесшапов", 1)})
+        {
+            return;
+        }
+        public override string ToString()
+        {
+            Array.ForEach(this.Students.ToArray(), el => Console.Write(el + " "));
+            return $"{this.GroupName}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Group[] groups = new Group[] { new Group(), new Group("БПИ2111", new List<Student>() { new Student("Иванов", 1), new Student("Петров", 1) })};
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream file = new FileStream("out1.txt", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(file, groups);
+            }
+            using (FileStream file = new FileStream("out1.txt", FileMode.OpenOrCreate))
+            {
+                Group[] gotGroup = (Group[])formatter.Deserialize(file);
+                Array.ForEach(gotGroup, el => Console.WriteLine(el));
+            }
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Group[]));
+            using (FileStream file =
+                new FileStream("out2.txt", FileMode.OpenOrCreate))
+            {
+                xmlSerializer.Serialize(file, groups);
+            }
+            using (FileStream file =
+                new FileStream("out2.txt", FileMode.OpenOrCreate))
+            {
+                Group[] gotGroup2 = (Group[])xmlSerializer.Deserialize(file);
+                Array.ForEach(gotGroup2, el => Console.WriteLine(el));
+            }
+            string json = JsonSerializer.Serialize<Group[]>(groups);
+            Group[] gotGroup3 = JsonSerializer.Deserialize<Group[]>(json);
+            Array.ForEach(gotGroup3, el => Console.WriteLine(el));
+        }
+    }
+}
---
+++ Module3/CW/Sem12/Task02/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace Task02
+{
+    [Serializable]
+    public class Human
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string Grade
+        {
+            get;
+            set;
+        }
+        public Human(string name)
+        {
+            Name = name;
+            Grade = "Миничелик";
+        }
+        public Human() : this("Иван")
+        {
+            return;
+        }
+        public override string ToString()
+        {
+            return $"{this.Name}, {this.Grade};";
+        }
+    }
+    [Serializable]
+    public class Professor : Human
+    {
+        public Professor(string name, string grade) : base(name)
+        {
+            Name = name;
+            Grade = grade;
+        }
+        public Professor() : base("Качалова")
+        {
+
+        }
+    }
+    [Serializable]
+    [XmlInclude(typeof(Human))]
+    public class Department
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public List<Human> Humans
+        {
+            get;
+            set;
+        }
+        public Department(string name, params Human[] humans)
+        {
+            Humans = new List<Human>();
+            Name = name;
+            foreach (var item in humans)
+            {
+                Humans.Add(new Professor(item.Name, item.Grade));
+            }
+        }
+        public Department() : this("Учебный офис", new Human[] { new Professor("МиниКачалова", "МиниБухгалтер") })
+        {
+            return;
+        }
+        public override string ToString()
+        {
+            Array.ForEach(this.Humans.ToArray(), el => Console.Write(el + " "));
+            return $"{this.Name}";
+        }
+    }
+    [Serializable]
+    [XmlInclude(typeof(Professor))]
+    public class University
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public Department[] Workers
+        {
+            get;
+            set;
+        }
+        public University(string name, Department[] departments)
+        {
+            Name = name;
+            Workers = departments;
+        }
+        public University() : this("ВШЭ", new Department[] { new Department("Учебный офис", new Human[] { new Professor("СуперКачалова", "СуперБухгалтер") }) })
+        {
+            return;
+        }
+        public override string ToString()
+        {
+            Array.ForEach(this.Workers, el => Console.WriteLine(el));
+            return $"{this.Name}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Department[] departments = new Department[] { new Department(), new Department("Учебный офис", new Human[] { new Professor("Ломоносов", "Профессор") }) };
+            University[] universities = new University[] { new University(), new University("МГУ", departments)};
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream file = new FileStream("out1.txt", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(file, universities);
+            }
+            using (FileStream file = new FileStream("out1.txt", FileMode.OpenOrCreate))
+            {
+                University[] gotGroup = (University[])formatter.Deserialize(file);
+                Array.ForEach(gotGroup, el => Console.WriteLine(el));
+            }
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(University[]));
+            using (FileStream file =
+                new FileStream("out2.txt", FileMode.OpenOrCreate))
+            {
+                xmlSerializer.Serialize(file, universities);
+            }
+            using (FileStream file =
+                new FileStream("out2.txt", FileMode.OpenOrCreate))
+            {
+                University[] gotGroup2 = (University[])xmlSerializer.Deserialize(file);
+                Array.ForEach(gotGroup2, el => Console.WriteLine(el));
+            }
+            string json = JsonSerializer.Serialize<University[]>(universities);
+            University[] gotGroup3 = JsonSerializer.Deserialize<University[]>(json);
+            Array.ForEach(gotGroup3, el => Console.WriteLine(el));
+        }
+    }
+}
